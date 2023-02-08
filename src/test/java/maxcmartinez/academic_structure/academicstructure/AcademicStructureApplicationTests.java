0743e1f75c284eaf19c9b8aedd2551be5705195b
Maximiliano Choque Martinez <maxc.m6811@gmail.com>
2023-02-08 12:50:20 -0400

    add title const

--- src/test/java/maxcmartinez/academic_structure/academicstructure/AcademicStructureApplicationTests.java
+++ src/test/java/maxcmartinez/academic_structure/academicstructure/AcademicStructureApplicationTests.java
@@ -5,9 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AcademicStructureApplicationTests {
-
+	private static final TITLE = "TEST";
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
